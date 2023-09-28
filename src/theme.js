22ef494e2cd7e53ef67f635b9124854498dac3bf
Collins Rotich <14885955+collinskrotich@users.noreply.github.com>
2023-09-28 15:41:25 +0300

    Finished creation of dark/light mode

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
+import { mode } from '@chakra-ui/theme-tools';
 
 // color design tokens
 export const tokens = (mode) => ({
@@ -128,5 +129,84 @@ export const themeSettings = (mode) => {
             ...(mode === 'dark' ? {
                 primary: {
                     main: colors.primary[500],
-                })
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100]
+                },
+                background: {
+                    default: colors.primary[500],
+                }
+            } : {
+                primary: {
+                    main: colors.primary[100],
+                },
+                secondary: {
+                    main: colors.greenAccent[500],
+                },
+                neutral: {
+                    dark: colors.grey[700],
+                    main: colors.grey[500],
+                    light: colors.grey[100]
+                },
+                background: {
+                    default: "#fcfcfc", // colors.primary[500],
+                },
+
+            }
+        )
+        },
+        typography: {
+            fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Source Sans Pro", "sans-serif"].join(""),
+                fontSize: 14,
+            },
+        }
+    }
+};
+
+export const ColorModeContext = createContext({
+    toggleColorMode: () => { }
+});
+
+export const useMode = () => {
+    const [mode, setMode] = useState('dark');
+
+    const colorMode = useMemo(
+        () => ({
+            toggleColorMode: () => 
+                setMode((prev) => (prev === 'light'? 'dark' : 'light')),
+        }),
+    )
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+    return [colorMode, theme];
 }
+
+    
